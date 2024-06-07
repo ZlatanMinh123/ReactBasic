c5cefe86105122ba5b6017f8f06d56a141a8dbe7
Your Name <you@example.com>
2024-06-08 03:16:58 +0700

    Cách sử dụng Function Component

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,47 +1,44 @@
 import React from "react";
 
-class ChildComponent extends React.Component {
-    state = {
-        firstName: "",
-        lastName: "",
-    };
+// class ChildComponent extends React.Component {
 
-    handleChangeFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value,
-        });
-    };
+//     render() {
+//         // console.log('>> check props: ', this.props)
+//         let {name, age, address, arrJobs} = this.props
+//         return (
+//             <>
+//                 <div className="job-lists">
+//                     {
+//                         arrJobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             );
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         );
+//     }
+// }
 
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value,
-        });
-    };
+const ChildComponent = (props) => {
+    let { name, age, address, arrJobs } = props;
+    return (
+        <>
+            <div className="job-lists">
+                {arrJobs.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            {item.title} - {item.salary}
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-       console.log('>>> check data input: ', this.state);
-    }
-
-    render() {
-        // console.log('>> check props: ', this.props)
-        let {name, age, address, arrJobs} = this.props
-        return (
-            <>
-                <div className="job-lists">
-                    {
-                        arrJobs.map((item, index) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-            </>
-        );
-    }
-}
 // Sd export để dùng file này bên trong file App.js
 export default ChildComponent;
