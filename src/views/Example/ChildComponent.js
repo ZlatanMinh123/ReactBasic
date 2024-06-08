4c07bf8d097f4b6c8ce364c9d983b418d71a15be
Your Name <you@example.com>
2024-06-08 17:24:14 +0700

    Làm nút Show Hide cách 1

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,44 +1,56 @@
 import React from "react";
 
-// class ChildComponent extends React.Component {
+class ChildComponent extends React.Component {
+    state = {
+        showJobs: false,
+    };
 
-//     render() {
-//         // console.log('>> check props: ', this.props)
-//         let {name, age, address, arrJobs} = this.props
-//         return (
-//             <>
-//                 <div className="job-lists">
-//                     {
-//                         arrJobs.map((item, index) => {
-//                             return (
-//                                 <div key={item.id}>
-//                                     {item.title} - {item.salary}
-//                                 </div>
-//                             );
-//                         })
-//                     }
-//                 </div>
-//             </>
-//         );
-//     }
-// }
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs,
+        });
+    };
+    render() {
+        let { arrJobs } = this.props;
+        let { showJobs } = this.state;
+        return (
+            <>
+                {showJobs === false && (
+                    // Khi bấm vào nút Show thì biến showJobs sẽ thành true
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>
+                            Show
+                        </button>
+                    </div>
+                )}
 
-const ChildComponent = (props) => {
-    let { name, age, address, arrJobs } = props;
-    return (
-        <>
-            <div className="job-lists">
-                {arrJobs.map((item, index) => {
-                    return (
-                        <div key={item.id}>
-                            {item.title} - {item.salary}
+                {showJobs && (
+                    // Biến showJobs = true nên đoạn code bên dưới sẽ thực hiện,
+                    // job-list sẽ được render ra.
+                    <>
+                        <div className="job-lists">
+                            {arrJobs.map((item, index) => {
+                                return (
+                                    <div key={item.id}>
+                                        {item.title} - {item.salary}
+                                    </div>
+                                );
+                            })}
                         </div>
-                    );
-                })}
-            </div>
-        </>
-    );
-};
+                        <div>
+                            {/* Bấm vào nút Hide lần nữa biến showJobs sẽ chuyển lại
+                                thành false
+                             */}
+                            <button onClick={() => this.handleShowHide()}>
+                                Hide
+                            </button>
+                        </div>
+                    </>
+                )}
+            </>
+        );
+    }
+}
 
 // Sd export để dùng file này bên trong file App.js
 export default ChildComponent;
